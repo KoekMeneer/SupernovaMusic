b5bf4f912060f208c6bdad975cb7b997fe3a6db8
KoekMeneer <91791070+KoekMeneer@users.noreply.github.com>
2023-01-17 18:24:03 +0100

    Updated for tModloader 1.4

---
+++ Common/SceneEffects/ModBossMusicSceneEffect.cs
@@ -0,0 +1,41 @@
+﻿using log4net.Repository.Hierarchy;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SupernovaMusic.Common.SceneEffects
+{
+	[Autoload(false)]
+	public class ModBossMusicSceneEffect : ModSceneEffect
+	{
+		private readonly string _musicName;
+
+		private readonly int _npcType;
+
+		public ModBossMusicSceneEffect(string musicName, ModNPC npc)
+			:this(musicName, npc.Type)
+		{
+		}
+
+		public ModBossMusicSceneEffect(string musicName, int npcType)
+		{
+			_musicName	= musicName;
+			_npcType	= npcType;
+		}
+
+		public override string Name => base.Name + $"/{_musicName}";
+
+		public override int Music => MusicLoader.GetMusicSlot(Mod, $"Assets/Music/{_musicName}");
+
+		public override bool IsSceneEffectActive(Player player)
+		{
+			// Check if any of our boss npc is active
+			//
+			if (NPC.AnyNPCs(_npcType))
+			{
+				return true;
+			}
+			return false;
+		}
+		public override SceneEffectPriority Priority => SceneEffectPriority.BossMedium;
+	}
+}
--- SupernovaMusic.cs
+++ SupernovaMusic.cs
@@ -1,28 +1,73 @@
 using Terraria;
 using Terraria.ModLoader;
+using Terraria.Audio;
+using SupernovaMusic.Common.SceneEffects;
 
 namespace SupernovaMusic
 {
 	public class SupernovaMusic : Mod
 	{
-		public override void UpdateMusic(ref int music, ref MusicPriority priority)
+		public Mod Supernova { get; private set; }
+
+		public SupernovaMusic()
+		{
+			MusicAutoloadingEnabled = true;
+		}
+
+		/// <summary>
+		/// Activates on load and registers all music files
+		/// </summary>
+		private void RegisterMusic()
+		{
+			Logger.Info("Loading Music...");
+
+			AddContent(new ModBossMusicSceneEffect(
+				"nyctophobia", 
+				GetSupernovaNpc("FlyingTerror")
+			));
+			Logger.Info("Registered: nyctophobia -> FlyingTerror");
+
+			AddContent(new ModBossMusicSceneEffect(
+				"anomaly",
+				GetSupernovaNpc("HarbingerOfAnnihilation")
+			));
+			Logger.Info("Registered: anomaly -> HarbingerOfAnnihilation");
+
+		}
+		public override void Load()
 		{
 			// Get the Supernova mod
-			Mod supernova = ModLoader.GetMod("Supernova");
+			Supernova = ModLoader.GetMod("Supernova");
 
 			// Check if we found the supernova mod
-			// If not we can't add music to the supernova mod
-			if (supernova != null)
+			//
+			if (Supernova == null)
 			{
-				// Check if the Flying Terror boss is active
-				if (NPC.AnyNPCs(supernova.NPCType("FlyingTerror")))
-				{
-					music = GetSoundSlot(SoundType.Music, "Sounds/Music/b_ft_nyctophobia");
-					priority = MusicPriority.BossLow;
-				}
+				Logger.Warn("This mod requires the Supernova mod. Make sure it is downloaded and active.");
+				return; // Don't load this mod
 			}
 
-			base.UpdateMusic(ref music, ref priority);
+			// Register mod music
+			RegisterMusic();
+			base.Load();
+		}
+
+		/// <summary>
+		/// Gets a Supernova mod npc by name.
+		/// </summary>
+		/// <param name="name">Name of the npc</param>
+		/// <returns>An NPC or null when none where found</returns>
+		private ModNPC? GetSupernovaNpc(string name)
+		{
+			// Try to find the npc
+			//
+			if (Supernova.TryFind(name, out ModNPC npc))
+			{
+				return npc;
+			}
+
+			Logger.Warn("GetSupernovaNpc(): Could not find npc with name '" + name + "'");
+			return null;
 		}
 	}
 }
