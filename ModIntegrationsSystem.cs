185a86845f286e53e1b45e4960d3c75b2a2f7cc6
RickLugtigheid <54893029+RickLugtigheid@users.noreply.github.com>
2023-12-31 15:52:39 +0100

    The music scenes are now handled by the main mod

---
+++ ModIntegrationsSystem.cs
@@ -0,0 +1,29 @@
+﻿using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace SupernovaMusic
+{
+	public class ModIntegrationsSystem : ModSystem
+	{
+		public override void PostSetupContent()
+		{
+			HandleIntegrationMusicDisplay();
+		}
+
+		private void HandleIntegrationMusicDisplay()
+		{
+			if (SupernovaMusic.Instance.musicDisplay == null)
+			{
+				return;
+			}
+
+			MusicDisplay_AddMusic("HarbingerOfAnnihilation", "theexiiedfeiiow");
+			MusicDisplay_AddMusic("FlyingTerror", "theexiiedfeiiow");
+		}
+		private void MusicDisplay_AddMusic(string songKey, string authorKey)
+		{
+			short slot = (short)MusicLoader.GetMusicSlot(Mod, $"Assets/Music/{songKey}");
+			SupernovaMusic.Instance.musicDisplay.Call("AddMusic", slot, Language.GetTextValue("Mods.SupernovaMusic.ModSupport.MusicDisplay.SongNames." + songKey), Language.GetTextValue("Mods.SupernovaMusic.ModSupport.MusicDisplay.Authors." + authorKey), Mod.DisplayName);
+		}
+	}
+}
