8accde4aa197a1372a5fa1d9cf989f9ae5438683
RickLugtigheid <54893029+RickLugtigheid@users.noreply.github.com>
2023-12-31 16:07:03 +0100

    Auto stash before merge of "main" and "origin/main"

--- SupernovaMusic.cs
+++ SupernovaMusic.cs
@@ -1,73 +1,31 @@
-using Terraria;
 using Terraria.ModLoader;
-using Terraria.Audio;
-using SupernovaMusic.Common.SceneEffects;
 
 namespace SupernovaMusic
 {
 	public class SupernovaMusic : Mod
 	{
-		public Mod Supernova { get; private set; }
+		public static SupernovaMusic Instance { get; private set; }
+
+		public Mod supernova = null;
+		public Mod musicDisplay = null;
 
 		public SupernovaMusic()
 		{
-			MusicAutoloadingEnabled = true;
+			Instance = this;
 		}
 
-		/// <summary>
-		/// Activates on load and registers all music files
-		/// </summary>
-		private void RegisterMusic()
-		{
-			Logger.Info("Loading Music...");
-
-			AddContent(new ModBossMusicSceneEffect(
-				"nyctophobia", 
-				GetSupernovaNpc("FlyingTerror")
-			));
-			Logger.Info("Registered: nyctophobia -> FlyingTerror");
-
-			AddContent(new ModBossMusicSceneEffect(
-				"anomaly",
-				GetSupernovaNpc("HarbingerOfAnnihilation")
-			));
-			Logger.Info("Registered: anomaly -> HarbingerOfAnnihilation");
-
-		}
 		public override void Load()
 		{
-			// Get the Supernova mod
-			Supernova = ModLoader.GetMod("Supernova");
-
-			// Check if we found the supernova mod
-			//
-			if (Supernova == null)
-			{
-				Logger.Warn("This mod requires the Supernova mod. Make sure it is downloaded and active.");
-				return; // Don't load this mod
-			}
-
-			// Register mod music
-			RegisterMusic();
-			base.Load();
+			supernova = null;
+			ModLoader.TryGetMod("SupernovaMod", out supernova);
+			musicDisplay = null;
+			ModLoader.TryGetMod("MusicDisplay", out musicDisplay);
 		}
-
-		/// <summary>
-		/// Gets a Supernova mod npc by name.
-		/// </summary>
-		/// <param name="name">Name of the npc</param>
-		/// <returns>An NPC or null when none where found</returns>
-		private ModNPC? GetSupernovaNpc(string name)
+		public override void Unload()
 		{
-			// Try to find the npc
-			//
-			if (Supernova.TryFind(name, out ModNPC npc))
-			{
-				return npc;
-			}
-
-			Logger.Warn("GetSupernovaNpc(): Could not find npc with name '" + name + "'");
-			return null;
+			supernova = null;
+			musicDisplay = null;
+			Instance = null;
 		}
 	}
 }
